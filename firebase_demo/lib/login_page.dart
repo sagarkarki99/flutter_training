478e0d1e238fa5107b2398299e63f44425a2a3c6
sagarkarki99 <sagarkarki184@gmail.com>
2022-04-28 09:48:22 +0545

    complete login functionality.

--- firebase_demo/lib/login_page.dart
+++ firebase_demo/lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_demo/home_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,62 +12,67 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool isPasswordVisible = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign in'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                hintText: 'Email',
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Email cannot be empty";
+                  } else if (!value.contains('@')) {
+                    return 'Please enter a valid email address';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                hintText: 'Password',
+              const SizedBox(height: 20),
+              TextFormField(
+                  controller: passwordController,
+                  obscureText: !isPasswordVisible,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        },
+                        icon: isPasswordVisible
+                            ? const Icon(Icons.remove_red_eye)
+                            : const Icon(
+                                Icons.no_accounts,
+                              )),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Password should not be empty.";
+                    }
+                    return null;
+                  }),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                child: const Text('Login'),
+                onPressed: () async {
+                  await _signInUser();
+                },
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              child: const Text('Login'),
-              onPressed: () async {
-                try {
-                  final userCredentials =
-                      await FirebaseAuth.instance.signInWithEmailAndPassword(
-                    email: emailController.text,
-                    password: passwordController.text,
-                  );
-                  print(userCredentials.user?.email);
-                } on FirebaseAuthException catch (e) {
-                  final errorMessage = _getErrorMessage(e);
-                  print(errorMessage);
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Text('Login Error'),
-                      content: Text(errorMessage),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Okay'),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -86,5 +92,80 @@ class _LoginPageState extends State<LoginPage> {
       default:
         return "Something is not right.Please try again later";
     }
+  }
+
+  void _showLoadingDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: const [
+              CircularProgressIndicator(),
+              const SizedBox(width: 20),
+              Text('signing in...'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _hideLoadingDialog() {
+    Navigator.of(context).pop();
+  }
+
+  bool _isUserValid() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    return isValid;
+  }
+
+  Future<void> _signInUser() async {
+    if (!_isUserValid()) {
+      return;
+    }
+    _showLoadingDialog();
+    _signInToFirebase();
+  }
+
+  Future<void> _signInToFirebase() async {
+    try {
+      final userCredentials =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      _hideLoadingDialog();
+      _goToHomePage();
+      print(userCredentials.user?.email);
+    } on FirebaseAuthException catch (e) {
+      _hideLoadingDialog();
+      final errorMessage = _getErrorMessage(e);
+      print(errorMessage);
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Login Error'),
+          content: Text(errorMessage),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Okay'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void _goToHomePage() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => MyHomePage(),
+      ),
+    );
   }
 }
